34d6c6ecba6a13de01e925a9546d6714f4a66fac
alosipov <alosipov@tpcorp.ru>
2019-01-23 14:00:51 +0300

    lesson 4

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -1,14 +1,25 @@
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 # Задание-1:
-# Дан список, заполненный произвольными целыми числами. 
+# Дан список, заполненный произвольными целыми числами.
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+orig_list = [1, 2, 4, 0]
+
+new_list = [i**2 for i in orig_list]
+print(new_list)
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruit_list_a = ['a', 'b', 'c', 'd']
+fruit_list_b = ['b', 'd', 'f', 'h']
+
+fruit_list_cross = [i for i in fruit_list_a if i in fruit_list_b]
+print(fruit_list_cross)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +27,20 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+import random
+
+lst = []
+for el in range(33):
+    lst.append(random.randint(-100, 100))
+
+lst_div_3 = [i for i in lst if i % 3 == 0]
+print(lst_div_3)
+
+lst_positiv = [i for i in lst if i >= 0]
+print(lst_positiv)
+
+lst_ndiv_4 = [i for i in lst if i % 4 != 0]
+print(lst_ndiv_4)
+
+
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,11 +20,34 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+import re
+
+with_re = re.findall(r'([a-z]+)[A-Z]+([a-z]+)', line) # если брать только символы вокруг символов в верхнем регистре
+lst = [j for i in with_re for j in i]
+print(lst)
+
+with_re = re.findall(r'[a-z]+', line) # просто все символы в нижнем регистре, тоже даёт верное решение
+print(with_re)
+
+temp = ''
+result = []
+
+for symbol in line:
+       if ord(symbol) in range(ord('a'), ord('z')):
+              temp = temp + symbol
+       if ord(symbol) in range(ord('A'), ord('Z')) and len(temp) > 0:
+              result.append(temp)
+              temp = ''
+
+if len(temp) > 0:
+       result.append(temp)
+
+print(result)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
@@ -45,9 +68,105 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+import re
+
+with_re = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(with_re)
+
+line_2 = 'GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec'
+print(line_2)
+
+with_re = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(with_re)
+
+temp = ''
+result = []
+small_count = 0
+big_count = 0
+
+for symbol in line_2:
+       #print(symbol, small_count, big_count, temp)
+       if ord(symbol) in range(ord('a'), ord('z')+1):
+              # закончились большие буквы, их было нужное кол-во и перед ними было нужное кол-во маленьких
+              if big_count > 2 and small_count == -1:
+                     result.append(temp[:big_count-2])
+                     temp = ''
+
+              # сброс флага маленьких букв, когда после больших начались снова маленькие буквы
+              if small_count == -1:
+                     small_count = 0
+              small_count += 1
+              big_count = 0
+              temp = ''
+
+       if ord(symbol) in range(ord('A'), ord('Z')+1):
+              big_count += 1
+              # установка флага маленьких букв, если перед большими было достаточное количество маленьких букв
+              # или сброс
+              if small_count >= 2 or small_count == -1:
+                     small_count = -1
+              else:
+                     small_count = 0
+              temp = temp + symbol
+
+print(result)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+
+with open('bigChislo.txt', 'w', encoding='UTF-8', ) as f:
+       f.write('')
+
+i = 0
+while i < 2500:
+       i += 1
+       cifra = random.randint(0,9)
+       with open('bigChislo.txt', 'a', encoding='UTF-8', ) as f:
+              f.write(str(cifra))
+
+repeated = {}
+prev_cifra = None
+current_repeat = 0
+is_repeat = False
+
+with open('bigChislo.txt', 'r', encoding='UTF-8') as f:
+    for line in f:
+       for cifra in line:
+              #print(cifra, prev_cifra, is_repeat, current_repeat)
+              if prev_cifra == cifra:
+                     if is_repeat:
+                            repeated[current_repeat].append(cifra)
+                     else:
+                            is_repeat = True
+                            repeated[current_repeat] = []
+                            repeated[current_repeat].append(cifra)
+                            repeated[current_repeat].append(cifra)
+              else:
+                     if is_repeat:
+                            is_repeat = False
+                            current_repeat += 1
+              prev_cifra = cifra
+
+count_repeated = [[len(values), values[0]] for values  in repeated.values()]
+
+max_key, max_values = max(count_repeated)
+max_count_repeated = [[i, j] for [i, j] in count_repeated if i == max_key]
+
+print('самые длинные последовательности одинаковых цифр:')
+i = 0
+for key, val in max_count_repeated:
+       i += 1
+       print(str(i)+')', key * val)
+
+
+
+
+
+
