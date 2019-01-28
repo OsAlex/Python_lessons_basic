485d20d769559f8cd362798188775b1626d87192
alosipov <alosipov@tpcorp.ru>
2019-01-28 18:24:04 +0300

    lesson 5

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,49 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+def create_dir(dirname):
+    try:
+        os.mkdir(dirname)
+    except Exception:
+        print('Something wrong with create ' + dirname)
+
+
+def create_dirs():
+    for i in range(1,10):
+        create_dir('dir_' + str(i))
+
+def remove_dir(dir):
+    try:
+        os.rmdir(dir)
+    except Exception:
+        print('Something wrong with remove ' + dir)
+
+def remove_dirs():
+    for i in range(1,10):
+        remove_dir('dir_' + str(i))
+
+
+create_dirs()
+remove_dirs()
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def print_dirs():
+    for item in os.listdir():
+        if os.path.isdir(item):
+            print(item)
+
+create_dirs()
+print_dirs()
+remove_dirs()
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy_file(source, dest):
+    with open(source, 'r', encoding="utf-8") as src, open(dest, 'w', encoding="utf-8") as dst: dst.write(src.read())
+
+copy_file(os.path.basename(__file__), os.path.basename(__file__) + '.copy')
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,39 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import hw05_easy as my_file_func
+
+menu = [
+    '1. Перейти в папку',
+    '2. Просмотреть содержимое текущей папки',
+    '3. Удалить папку',
+    '4. Создать папку',
+]
+
+while(True):
+    for item in menu:
+        print(item)
+
+    try:
+        selected = int(input())
+    except Exception:
+        print('Good Bay!')
+        break
+
+    if selected == 1 or selected == 3 or selected == 4:
+        print('Input dir name: ')
+        dirname = input()
+
+    if selected == 1:
+        try:
+            os.chdir(dirname)
+        except Exception as e:
+            print('Something wrong with chdir ' + str(dirname) + ': ' + str(e))
+    elif selected == 2:
+        my_file_func.print_dirs()
+    elif selected == 3:
+        my_file_func.remove_dir(dirname)
+    elif selected == 4:
+        my_file_func.create_dir(dirname)
